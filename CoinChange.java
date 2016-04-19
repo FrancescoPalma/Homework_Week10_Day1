c483aa0b43ffd97c34183bc2fd411f43045937d0
Francesco <fpfrancescopalma7@gmail.com>
2016-04-19 08:44:30 +0100

    Adds completed homework

--- CoinChange.java
+++ CoinChange.java
@@ -1,16 +1,76 @@
+import java.math.BigDecimal;
+
 public class CoinChange {
 
-  int amount;
-  int[] coins = {0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 10, 50};
-
-  public void giveChange(double amount) {
-    int onePenny = twoPence = fivePence = tenPence = twentyPence = fiftyPence = onePound = twoPounds = fivePounds = tenPounds = twentyPounds = fiftyPounds = 0;
-
+  public static void main(String[] args) {
+    CoinChange coinChange = new CoinChange();
+    coinChange.giveChange(3.56);
   }
 
-}
+  int onePenny = 0;
+  int twoPence = 0;
+  int fivePence = 0;
+  int tenPence = 0;
+  int twentyPence = 0;
+  int fiftyPence = 0;
+  int onePound = 0;
 
-public static void main(String[] args) {
-  CoinChange coinChange = new CoinChange;
-  coinChange.giveChange(46.89);
+  public void giveChange(double amount) {
+
+    System.out.printf("Amount: %s \n", amount);
+
+    do {
+
+      amount = Math.round(amount * 100.0)/100.0;
+
+      while (amount >= 1 && amount < 5) {
+        amount -= 1;
+        onePound++;
+      }
+
+      while (amount >= 0.5 && amount < 1) {
+        amount -= 0.5;
+        fiftyPence++;
+      }
+
+      while (amount >= 0.2 && amount < 0.5) {
+        amount -= 0.2;
+        twentyPence++;
+      }
+
+      while (amount >= 0.1 && amount < 0.2) {
+        amount -= 0.1;
+        tenPence++;
+      }
+
+      while (amount >= 0.05 && amount < 0.1) {
+        amount -= 0.05;
+        fivePence++;
+      }
+
+      while (amount >= 0.02 && amount < 0.05) {
+        amount -= 0.02;
+        fivePence++;
+      }
+
+      while (amount >= 0.01 && amount < 0.02) {
+        amount -= 0.01;
+        onePenny++;
+      }
+
+    } while(amount > 0);
+
+    this.printCoinChange();
+  }
+
+  public void printCoinChange() {
+    System.out.printf("%s £1 coins \n", onePound);
+    System.out.printf("%s £0.5 coins \n", fiftyPence);
+    System.out.printf("%s £0.2 coins \n", twentyPence);
+    System.out.printf("%s £0.1 coins \n", tenPence);
+    System.out.printf("%s £0.05 coins \n", fivePence);
+    System.out.printf("%s £0.02 coins \n", twoPence);
+    System.out.printf("%s £0.01 coins \n", onePenny);
+  }
+
 }
